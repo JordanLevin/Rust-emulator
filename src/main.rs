4e5b82e8cb90c953d7f1b53a8b3451de110d31f3
JordanLevin <jlevin38@binghamton.edu>
2019-04-07 01:03:23 -0400

    began setup for executing code

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,44 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::io;
-use std::fmt;
+use std::collections::HashMap;
 
 fn main() {
+
     disassemble(String::from("../roms/15PUZZLE"));
 }
 
+#[allow(non_snake_case)]
 fn disassemble(path: String) -> std::io::Result<()>{
+    let mut PROGRAM: Vec<Vec<u8>> = Vec::new();
+    let mut PC = 0;
+    let mut REGS: Vec<u8> = vec!([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+    let mut ADDR: u16 = 0;
+    let mut STACK: Vec<[u8; 48]> = Vec::new();
+    let mut DELAY_TIMER: u8 = 0;
+    let mut SOUND_TIMER: u8 = 0;
+    let mut MEMORY: [u8: 4096] = [0; 4096];
+
     let mut input = File::open(path)?;
     let mut buffer = Vec::new();
-    let mut i= 0;
-    let mut opcodes: Vec<Vec<u8>> = Vec::new();
 
     //read until end of file
     input.read_to_end(&mut buffer)?;
 
+    //Setup instructions for running program
+    let mut i = 0;
     while i < buffer.len() {
         let mut opcode: Vec<u8> = vec![0,0,0,0];
         opcode[0] = buffer[i] >> 4;
         opcode[1] = (buffer[i] << 4) >> 4;
         opcode[2] = buffer[i+1] >> 4;
         opcode[3] = (buffer[i+1] << 4) >> 4;
-        //print!("{:x}", opcode[0]);
-        //print!("{:x}", opcode[1]);
-        //print!("{:x}", opcode[2]);
-        //println!("{:x}", opcode[3]);
-        opcodes.push(opcode);
+        PROGRAM.push(opcode);
         i+=2;
     }
 
     let mut addr = 0;
-    for opcode in opcodes {
+    loop {
+        let opcode = &PROGRAM[PC/4];
         print!("{1:00$X}: ", 4, addr);
         addr += 2;
         match opcode[0] {
@@ -40,19 +47,9 @@ fn disassemble(path: String) -> std::io::Result<()>{
                     0xE => match opcode[3] {
                         0x0 => println!("DISPLAY CLEAR"),
                         0xE => println!("RETURN"),
-                        _ => {
-                            print!("{:x}", opcode[0]);
-                            print!("{:x}", opcode[1]);
-                            print!("{:x}", opcode[2]);
-                            println!("{:x}", opcode[3]);
-                        },
+                        _ => println!("INVALID OPCODE"),
                     },
-                    _ => {
-                        print!("{:x}", opcode[0]);
-                        print!("{:x}", opcode[1]);
-                        print!("{:x}", opcode[2]);
-                        println!("{:x}", opcode[3]);
-                    },
+                    _ => println!("INVALID OPCODE"),
                 },
                 _ => println!("CALL {:x}{:x}{:x}", opcode[1], opcode[2], opcode[3]),
             },
@@ -109,6 +106,7 @@ fn disassemble(path: String) -> std::io::Result<()>{
                 println!("{:x}", opcode[3]);
             },
         }
+        PC = PC+1;
     }
 
     //Return ok so the compiler doesnt complain about the result
