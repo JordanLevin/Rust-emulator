1d13e184eeb10f6d11618b882723b9e01d45f754
JordanLevin <jlevin38@binghamton.edu>
2019-04-07 05:36:59 -0400

    GOT THINGS TO RENDER, need to fix key inputs and a bunch of memory stuff still

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,65 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::{thread, time};
+use std::time::Duration;
+
+//Random vals
+extern crate rand;
+use rand::Rng;
+
+//SDL2 imports
+extern crate sdl2; 
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::rect;
+ 
 
 fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+ 
+    let window = video_subsystem.window("CHIP8 Emulator", 640, 320)
+        .position_centered()
+        .build()
+        .unwrap();
+ 
+    let mut canvas = window.into_canvas().build().unwrap();
+ 
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
 
-    disassemble(String::from("../roms/PONG"));
+    disassemble(String::from("../roms/PONG"), &mut canvas, &sdl_context);
+}
+
+fn draw(canvas: &mut sdl2::render::WindowCanvas, sdl_context: &sdl2::Sdl,
+       SCREEN: &[[bool; 64]; 32]){
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    for event in event_pump.poll_iter() {
+        match event {
+            Event::Quit {..} |
+            Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                std::process::exit(0);
+            },
+            _ => {}
+        }
+    }
+    // The rest of the game loop goes here...
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    for row in 0..32{
+        for col in 0..64{
+            if SCREEN[row][col]{
+                let r = sdl2::rect::Rect::new(10*row as i32, 10*col as i32,10,10);
+                canvas.fill_rect(r);
+            }
+        }
+    }
+
+    canvas.present();
+    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
 }
 
 fn debug_print(opcode : &Vec<u8>, src: i32){
@@ -24,18 +80,19 @@ fn transform_opcode(pc: usize, mem: &[u8; 4096]) -> Vec<u8> {
 }
 
 #[allow(non_snake_case)]
-fn disassemble(path: String) -> std::io::Result<()>{
+fn disassemble(path: String, canvas: &mut sdl2::render::WindowCanvas, sdl_context: &sdl2::Sdl)
+    -> std::io::Result<()>{
     let mut PROGRAM: Vec<Vec<u8>> = Vec::new();
     let mut PC: u16 = 0x200;
     let mut SP: u16 = 0xEA0;
     let mut REGS: [u8; 16] = [0;16];
     let mut ADDR: u16 = 0;
-    let mut STACK: Vec<[u8; 48]> = Vec::new();
+    let mut STACK: Vec<u16> = Vec::new();
     let mut DELAY_TIMER: u8 = 0;
     let mut SOUND_TIMER: u8 = 0;
     let mut MEMORY: [u8; 4096] = [0; 4096];
-    let mut SCREAN: [[bool; 64]; 32] = [[false; 64]; 32];
-    let mut DEBUG: u16 = 0;
+    let mut SCREEN: [[bool; 64]; 32] = [[false; 64]; 32];
+    //let mut DEBUG: u16 = 0;
 
 
     let mut input = File::open(path)?;
@@ -44,109 +101,169 @@ fn disassemble(path: String) -> std::io::Result<()>{
     //read until end of file
     input.read_to_end(&mut buffer)?;
 
-    //Setup instructions for running program
+    //Setup instructions to memory for running program
     let mut i = 0;
     while i < buffer.len(){
-        //let mut opcode: Vec<u8>  vec![0,0,0,0];
-        //opcode[0] = buffer[i] >> 4;
-        //opcode[1] = (buffer[i] << 4) >> 4;
-        //opcode[2] = buffer[i+1] >> 4;
-        //opcode[3] = (buffer[i+1] << 4) >> 4;
-        //PROGRAM.push(opcode);
         MEMORY[PC as usize+i] = buffer[i];
         MEMORY[PC as usize+i+1] = buffer[i+1];
         i+=2;
     }
-    //println!("{:?}", MEMORY.to_vec());
 
-    let mut addr = 0;
     loop {
-        DEBUG = DEBUG+1;
-        if DEBUG > 500{
-            break;
-        }
-        //let opcode = &PROGRAM[(PC/4) as usize];
         let opcode = transform_opcode(PC as usize, &MEMORY);
         print!("{1:00$X}: ", 4, PC);
-        addr += 2;
         match opcode[0] {
             0x0 => match opcode[1] {
                 0x0 => match opcode[2] {
                     0xE => match opcode[3] {
-                        0x0 => println!("DISPLAY CLEAR"),
-                        0xE => println!("RETURN"),
+                        0x0 => {
+                            for row in 0..32{
+                                for col in 0..64{
+                                    SCREEN[row][col] = false;
+                                }
+                            }
+                        },
+                        0xE => {
+                            PC = STACK[STACK.len()-1];
+                            STACK.pop();
+                        },
                         //_ => debug_print(&opcode, 1),
-                        _ => println!("CALL {:x}{:x}{:x}", opcode[1], opcode[2], opcode[3]),
+                        _ => print!("CALL {:x}{:x}{:x}", opcode[1], opcode[2], opcode[3]),
                     },
                     //_ => debug_print(&opcode, 2),
-                    _ => println!("CALL {:x}{:x}{:x}", opcode[1], opcode[2], opcode[3]),
+                    _ => print!("CALL {:x}{:x}{:x}", opcode[1], opcode[2], opcode[3]),
                 },
-                _ => println!("CALL {:x}{:x}{:x}", opcode[1], opcode[2], opcode[3]),
+                _ => print!("CALL {:x}{:x}{:x}", opcode[1], opcode[2], opcode[3]),
             },
             0x1 => {
-                println!("GOTO {:x}{:x}{:x}", opcode[1], opcode[2], opcode[3]);
                 PC = (opcode[1] as u16) << 8;
                 PC |= ((opcode[2] as u16) << 4);
                 PC |= (opcode[3] as u16);
+                PC = PC - 2; //Account for adding 2 to PC at end of loop
             },
                 
-            0x2 => println!("CALL AT ADDR {:x}{:x}{:x}", opcode[1], opcode[2], opcode[3]),
+            0x2 => {
+                STACK.push(PC);
+                PC = (opcode[1] as u16) << 8;
+                PC |= ((opcode[2] as u16) << 4);
+                PC |= (opcode[3] as u16);
+                PC = PC - 2; //Account for adding 2 to PC at end of loop
+            },
             0x3 => if REGS[opcode[1] as usize] == ((opcode[2] << 4) | opcode[3]){
-                    PC = PC + 4;
+                    PC = PC + 2;
                 },
             0x4 => if REGS[opcode[1] as usize] != ((opcode[2] << 4) | opcode[3]){
-                    PC = PC + 4;
+                    PC = PC + 2;
                 },
             0x5 => if REGS[opcode[1] as usize] != REGS[opcode[2] as usize]{
-                    PC = PC + 4;
+                    PC = PC + 2;
                 },
-            0x6 => REGS[opcode[1] as usize] = ((opcode[2] << 4) | opcode[3]),
-            0x7 => REGS[opcode[1] as usize] =
-                REGS[opcode[1] as usize] + ((opcode[2] << 4) | opcode[3]),
+            0x6 => {
+                REGS[opcode[1] as usize] = ((opcode[2] << 4) | opcode[3]);
+                },
+            0x7 => {
+                REGS[opcode[1] as usize] =
+                REGS[opcode[1] as usize] + ((opcode[2] << 4) | opcode[3]);
+                },
             0x8 => match opcode[3] {
-                    0x0 => println!("V{:x} = V{:x}", opcode[1], opcode[2]),
-                    0x1 => println!("V{:x} = V{:x}|V{:x}", opcode[1], opcode[1], opcode[2]),
-                    0x2 => println!("V{:x} = V{:x}&V{:x}", opcode[1], opcode[1], opcode[2]),
-                    0x3 => println!("V{:x} = V{:x}^V{:x}", opcode[1], opcode[1], opcode[2]),
-                    0x4 => println!("V{:x} += V{:x}", opcode[1], opcode[2]),
-                    0x5 => println!("V{:x} -= V{:x}", opcode[1], opcode[2]),
-                    0x6 => println!("V{:x}>>=1", opcode[1]),
-                    0x7 => println!("V{:x} = V{:x}-V{:x}", opcode[1], opcode[2], opcode[1]),
-                    0xe => println!("V{:x}<<=1", opcode[1]),
-                    _ => debug_print(&opcode, 3),
-                    },
-            0x9 => println!("IF V{:x} == V{:x}", opcode[1], opcode[2]),
-            0xA => println!("I = {:x}{:x}{:x}", opcode[1], opcode[2], opcode[3]),
-            0xB => println!("PC = V0 + {:x}{:x}{:x}", opcode[1], opcode[2], opcode[3]),
-            0xC => println!("V{:x} = rand()&{:x}{:x}", opcode[1], opcode[2], opcode[3]),
-            0xD => println!("draw(V{:x},V{:x},{:x})", opcode[1], opcode[2], opcode[3]),
+                0x0 => REGS[opcode[1] as usize] = REGS[opcode[2] as usize],
+                0x1 => REGS[opcode[1] as usize] |= REGS[opcode[2] as usize],
+                0x2 => REGS[opcode[1] as usize] &= REGS[opcode[2] as usize],
+                0x3 => REGS[opcode[1] as usize] ^= REGS[opcode[2] as usize],
+                0x4 => REGS[opcode[1] as usize] = REGS[opcode[1] as usize] + REGS[opcode[2] as usize], //TODO carry
+                0x5 => REGS[opcode[1] as usize] = REGS[opcode[1] as usize] - REGS[opcode[2] as usize], //TODO carry
+                0x6 => {
+                    REGS[0xF] = (REGS[opcode[1] as usize] & 1);
+                    REGS[opcode[1] as usize] >>= 1;
+                },
+                0x7 => REGS[opcode[1] as usize] = REGS[opcode[2] as usize] - REGS[opcode[1] as usize], //TODO borrow bit
+                0xe => {
+                    REGS[0xF] = (REGS[opcode[1] as usize] & 1);
+                    REGS[opcode[1] as usize] <<= 1;
+                },
+                _ => debug_print(&opcode, 3),
+            },
+            0x9 => if REGS[opcode[1] as usize] == REGS[opcode[2] as usize]{
+                    PC = PC + 2;
+                },
+            0xA => {
+                ADDR = (opcode[1] as u16) << 8;
+                ADDR |= ((opcode[2] as u16) << 4);
+                ADDR |= (opcode[3] as u16);
+            },
+            0xB => {
+                PC = (opcode[1] as u16) << 8;
+                PC |= ((opcode[2] as u16) << 4);
+                PC |= (opcode[3] as u16);
+                PC = PC + REGS[0] as u16;
+                PC = PC - 2; //Account for adding 2 to PC at end of loop
+            },
+            0xC => REGS[opcode[1] as usize] = 
+                rand::random::<u8>()&((opcode[1] << 4) | opcode[2]),
+            0xD => {
+                //println!("draw(V{:x},V{:x},{:x})", opcode[1], opcode[2], opcode[3]);
+                let mut x = REGS[opcode[1] as usize] as usize;
+                let mut y = REGS[opcode[2] as usize] as usize;
+                let mut addr = ADDR as usize;
+                for i in 0..opcode[3] as usize{
+                    let mut row = MEMORY[addr];
+                    for _ in 0..8{
+                        if y >= SCREEN.len() || x >= SCREEN[0].len(){
+                            println!("{0} {1}", x, y);
+                            continue;
+                        }
+                        let prev = SCREEN[y][x];
+                        SCREEN[y][x] = ((row & 1) != 0);
+                        if SCREEN[y][x] != prev{
+                            REGS[0xF] = 1;
+                        }
+                        row >>= 1;
+                        x = x+1;
+                    }
+                    y = y+1;
+                    x = x-8;
+                    addr = addr+1;
+                }
+            },//TODO, thats a big boy, verify if it works
             0xE => match opcode[2]{
-                0x9 => println!("IF key() == V{:x}", opcode[1]),
-                0xA => println!("IF key() != V{:x}", opcode[1]),
+                0x9 => print!("IF key() == V{:x}", opcode[1]),
+                0xA => print!("IF key() != V{:x}", opcode[1]),
                 _ => debug_print(&opcode, 4),
             },
             0xF => match opcode[2]{
                 0x0 => match opcode[3]{
-                    0x7 => println!("V{:x} = get_delay()", opcode[1]),
-                    0xA => println!("V{:x} = get_key()", opcode[1]),
+                    0x7 => REGS[opcode[1] as usize] = DELAY_TIMER,
+                    0xA => print!("V{:x} = get_key()", opcode[1]),
                     _ => debug_print(&opcode, 5),
                 },
                 0x1 => match opcode[3]{
-                    0x5 => println!("delay_timer(V{:x})", opcode[1]),
-                    0x8 => println!("sound_timer(V{:x})", opcode[1]),
-                    0xE => println!("I += V{:x}", opcode[1]),
+                    0x5 => DELAY_TIMER = REGS[opcode[1] as usize],
+                    0x8 => SOUND_TIMER = REGS[opcode[1] as usize],
+                    0xE => ADDR = ADDR + REGS[opcode[1] as usize] as u16,
                     _ => debug_print(&opcode, 6),
                 },
-                0x2 => println!("I=spride_addr[V{:x}]", opcode[1]),
-                0x3 => println!("set_BCD(V{:x})", opcode[1]),
-                0x5 => println!("reg_dump(V{:x}, &I)", opcode[1]),
-                0x6 => println!("reg_load(V{:x}, &I)", opcode[1]),
+                0x2 => print!("I=spride_addr[V{:x}]", opcode[1]),
+                0x3 => print!("set_BCD(V{:x})", opcode[1]),
+                0x5 => print!("reg_dump(V{:x}, &I)", opcode[1]),
+                0x6 => print!("reg_load(V{:x}, &I)", opcode[1]),
                 _ => debug_print(&opcode, 7),
             },
             _ => debug_print(&opcode, 8),
         }
- 
+
+        let frametime = time::Duration::from_millis(16);
+        thread::sleep(frametime);
+
+        DELAY_TIMER = DELAY_TIMER+1;
+        if DELAY_TIMER >= 60{
+            draw(canvas, sdl_context, &SCREEN);
+            //REFRESH
+            DELAY_TIMER = 0;
+        }
         PC = PC+2;
+
+
+        println!("");
     }
 
     //Return ok so the compiler doesnt complain about the result
